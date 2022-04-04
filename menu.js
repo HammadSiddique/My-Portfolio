eab8e3e4c4f7fb09de0530723e8b693db250ea8b
yorsyboy <adekunletaiwo43@yahoo.com>
2022-04-04 15:48:16 +0100

    Add Eslint linters and fix pop up

--- menu.js
+++ menu.js
@@ -2,14 +2,20 @@ const mainMenu = document.querySelector('.mobile-menu');
 const closeMenu = document.querySelector('.close-button');
 const openMenu = document.querySelector('.button');
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+function show() {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
 }
 
-function close(){
-    mainMenu.style.top = '-100%';
+function close() {
+  mainMenu.style.top = '-100%';
 }
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+document.querySelectorAll('.pop-nav').forEach((link) => {
+  link.addEventListener('click', () => {
+    document.getElementById('mobile-nav-menu').style.display = 'none';
+  });
+});
