9db928ad51e3a51a9b7df5f66f997ebe9281f8b7
HammadSiddique <hammadsiddique01@gmail.com>
2022-04-06 14:09:12 +0500

    Add popup windows for projects

--- script.js
+++ script.js
@@ -20,105 +20,107 @@ document.querySelectorAll('.pop-nav').forEach((link) => {
   });
 });
 
-const projectsPopup = [
+const projectname = document.querySelector('.projectname');
+const workSections = document.querySelector('.project-section');
+const modal = document.querySelector('.modal-example');
+const modalImg = document.querySelector('.modal-img');
+const company = document.querySelector('.company');
+const role = document.querySelector('.role');
+const year = document.querySelector('.year');
+
+const projectsList = [
   {
     name: 'Tonic',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    image: 'images/Snapshoot-Portfolio.svg',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    imageLink: 'images/Snapshoot-Portfolio.svg',
     technologies: ['html', 'css', 'javascript'],
-    livelink: 'https://hammadsiddique.github.io/My-Portfolio/',
-    sourceLink: 'https://github.com/HammadSiddique/My-Portfolio',
-},
-
+    role: 'Back end dev',
+    company: 'Canopy',
+    year: '2015',
+    sourceLink: '#',
+    demoLink: '#',
+  },
   {
     name: 'Multi-post stories',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    image: 'images/Snapshoot-Portfolio(1).svg',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    imageLink: 'images/Snapshoot-Portfolio-(1).svg',
     technologies: ['html', 'css', 'javascript'],
-    livelink: 'https://hammadsiddique.github.io/My-Portfolio/',
-    sourceLink: 'https://github.com/HammadSiddique/My-Portfolio',
-},
-
+    role: 'Full Stack Dev',
+    company: 'Canopy',
+    year: '2015',
+    sourceLink: '#',
+    demoLink: '#',
+  },
   {
     name: 'Tonic',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    image: 'images/Snapshoot-Portfolio(2).svg',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. ',
+    imageLink: 'images/Snapshoot-Portfolio-(2).svg',
     technologies: ['html', 'css', 'javascript'],
-    livelink: 'https://hammadsiddique.github.io/My-Portfolio/',
-    sourceLink: 'https://github.com/HammadSiddique/My-Portfolio',
-},
+    role: 'Back end Dev',
+    company: 'Canopy',
+    year: '2015',
+    sourceLink: '#',
+    demoLink: '#',
+  },
 ];
 
-const projectCards = document.querySelector('.project-cards .cards');
-
-for (let i = 0; i < projectsPopup.length; i += 1){
-  const mainCard = document.createElement('div');
-  mainCard.classList.add('card');
-  projectCards.append(mainCard);
-
-  const projectName = document.createElement('h3');
-  projectName.textContent = projectsPopup[i].name;
-  projectCards.append(projectName)
-
-  const projectImg = document.createElement ('img');
-  projectImg.setAttribute('src', projectsPopup[i].image);
-  mainCard.append(projectImg);
-
-  const projectContent = document.createElement('div');
-  projectContent.classList.add('content');
-  projectCards.append(projectContent);
-
-  const projectDescription = document.createElement('p');
-  projectDescription.textContent = projectsPopup[i].description;
-  projectContent.append(projectDescription);
-
-  const projectButton = document.createElement('button');
-  projectButton.textContent='See Project';
-  projectContent.append(projectButton);
-
-  projectButton.addEventListener('click', () => {
-    if (window.screen.width < 768) {
-      const mobileProjectPopupWindow = document.querySelector('.mobile-project-popup-window');
-      const mobileProjectPopupWindowImage = document.querySelector('.mobile-project-popup-window img ');
-      const mobileProjectPopupWindowName = document.querySelector('.mobile-project-popup-window h3');
-      const mobileProjectPopupWindowTechnologies = document.querySelector('.mobile-project-popup-window ul');
-      const mobileProjectPopupWindowDescription = document.querySelector('.mobile-project-popup-window .description');
-      const mobileProjectPopupWindowSeeLive = document.querySelector('.mobile-project-popup-window .see-live');
-      const mobileProjectPopupWindowSeeSource = document.querySelector('.mobile-project-popup-window .see-source');
-      const mobileProjectPopupWindowCloseBtn = document.querySelector('.mobile-project-popup-window .close-popup-window');
-
-      mobileProjectPopupWindowImage.src = projectCards[i].image;
-      mobileProjectPopupWindowName.textContent = projectCards[i].name;
-      mobileProjectPopupWindowTechnologies.innerHTML = '';
-      mobileProjectPopupWindowDescription.textContent = projectCards[i].description;
-      mobileProjectPopupWindowSeeLive.href = projectCards[i].livelink;
-      mobileProjectPopupWindowSeeSource.href = projectCards[i].sourceLink;
-      mobileProjectPopupWindow.classList.remove('display-none');
-      mobileProjectPopupWindowCloseBtn.addEventListener('click', () => {
-        mobileProjectPopupWindow.classList.add('display-none');
-        document.querySelector('body').style.overflow = 'scroll';
-      });
-    } else {
-      const desktopProjectPopupWindow = document.querySelector('.desktop-project-popup-window');
-      const desktopProjectPopupWindowImage = document.querySelector('.desktop-project-popup-window img');
-      const desktopProjectPopupWindowName = document.querySelector('.desktop-project-popup-window h3');
-      const desktopProjectPopupWindowTechnologies = document.querySelector('.desktop-project-popup-window ul');
-      const desktopProjectPopupWindowDescription = document.querySelector('.desktop-project-popup-window .description');
-      const desktopProjectPopupWindowSeeLive = document.querySelector('.desktop-project-popup-window .see-live');
-      const desktopProjectPopupWindowSeeSource = document.querySelector('.desktop-project-popup-window .see-source');
-      const desktopProjectPopupWindowCloseBtn = document.querySelector('.desktop-project-popup-window .close-popup-window');
-
-      desktopProjectPopupWindowImage.src = projectCards[i].image;
-      desktopProjectPopupWindowName.textContent = projectCards[i].name;
-      desktopProjectPopupWindowTechnologies.innerHTML = '';
-      desktopProjectPopupWindowDescription.textContent = projectCards[i].description;
-      desktopProjectPopupWindowSeeLive.href = projectCards[i].livelink;
-      desktopProjectPopupWindowSeeSource.href = projectCards[i].sourceLink;
-      desktopProjectPopupWindow.classList.remove('desktop-display-none');
-      desktopProjectPopupWindowCloseBtn.addEventListener('click', () => {
-        desktopProjectPopupWindow.classList.add('desktop-display-none');
-        document.querySelector('body').style.overflow = 'scroll';
-      });
-    }
-    document.querySelector('body').style.overflow = 'hidden';
+workSections.innerHTML = '';
+for (let i = 0; i < projectsList.length; i += 1) {
+  let languages = '';
+  projectsList[i].technologies.forEach((language) => {
+    languages += `
+      <li><a href='' class='skill' title='${language}' >${language}</a></li>
+    `;
+  });
+  workSections.innerHTML += `
+      <div class='projects'>
+          <div class='img-web'>
+            <img src='${projectsList[i].imageLink}' alt='work 1' class='' />
+          </div>
+          <div class='project-description'>
+            <h1 class='title'>${projectsList[i].name}</h1>
+            <div class='role'>
+              <div class="role-name">${projectsList[i].company}</div>
+                <div class="role-dot"></div>
+                <div class="role-job">${projectsList[i].role}</div>
+                <div class="role-dot"></div>
+                <div class="role-year">${projectsList[i].year}</div>
+            </div>
+            <div class='project-details'>
+              <p>
+                ${projectsList[i].description}
+              </p>
+              <ul class='skills'>
+                ${languages}
+              </ul>
+              <div classs="button-click">
+                <button type='button' class='btn btn-enabled project-modal-${i}'>See project</button>
+              </div>
+            </div>
+          </div>
+    </div>
+  `;
 }
+
+for (let i = 0; i < projectsList.length; i += 1) {
+  document.querySelector(`.project-modal-${i}`).addEventListener('click', () => {
+    projectname.innerHTML = projectsList[i].name;
+    modalImg.src = projectsList[i].imageLink;
+    company.innerHTML = projectsList[i].company;
+    role.innerHTML = projectsList[i].role;
+    year.innerHTML = projectsList[i].year;
+
+    document.querySelector('.project-section').classList.add('blur');
+    document.querySelector('.fixed-navbar').classList.add('blur');
+    document.querySelector('.page').classList.add('blur');
+    document.querySelector('.about').classList.add('blur');
+    document.querySelector('.contact-section').classList.add('blur');
+    modal.classList.add('show');
+  });
+}
+  document.querySelector('.closeModal').addEventListener('click', () => {
+  modal.style.display = 'none';
+  window.location.reload();
+  document.querySelector('.header').classList.remove('blur');
+  document.querySelector('.showcase').classList.remove('blur');
+});
